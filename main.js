b04d454437db2c70c348a535d517fd93da76bd04
unknown <paul.garay@codeimmersives.com>
2023-03-27 21:20:17 -0400

    removed whitespace in main.js. changed queryselector to getelementbyid.

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ const groups = {
     parries : ['./sounds/prima.mp3','./sounds/seconda.mp3','./sounds/terza.mp3','./sounds/quarta.mp3','./sounds/quinta.mp3','./sounds/sesta.mp3','./sounds/septima.mp3'],
     footwork : ["./sounds/advance.mp3","./sounds/stepBack.mp3","./sounds/stepLeft.mp3","./sounds/stepRight.mp3","./sounds/advanceLeft.mp3","./sounds/advanceRight.mp3","./sounds/stepBackLeft.mp3","./sounds/stepBackRight.mp3","./sounds/passeAvant.mp3","./sounds/passeArrier.mp3"]
 }
-
 const selectIndex = (arr) => {
     let selection = Math.floor(Math.random() * arr.length);
     return selection;
-};
-
+}
 let itemList = [];
 let toBePlayed = [];
 let index = 0;
-
 const buildList = () => {
     for(const item of selectedItems){
         if(item.checked){
@@ -21,7 +18,6 @@ const buildList = () => {
         }
     }
 }
-
 const randomize = (arr) => {
     while(toBePlayed.length < arr.length){
         let index = selectIndex(arr);
@@ -31,13 +27,11 @@ const randomize = (arr) => {
             toBePlayed.push(arr[index]);
         };
     };
-};
-
+}
 const playSound = (file) => {
     audio = new Audio(file);
     audio.play()
 }
-
 const queue = () => {
     console.log(toBePlayed);
     audio = new Audio(toBePlayed[index]);
@@ -51,15 +45,12 @@ const queue = () => {
         }, 2000
     )
 }
-
 const playList = (arr) => {
     toBePlayed = [];
     randomize(arr);
     queue();
 }
-
-
-document.querySelector('#shuffle').addEventListener("click", function(event) {event.preventDefault();buildList();index = 0;playList(itemList)});
-document.querySelector('#shuffle').addEventListener("touchstart", function(event) {event.preventDefault();buildList();index = 0;playList(itemList)});
-document.querySelector('#replay').addEventListener("click", function(event) {event.preventDefault();index = 0;queue()});
-document.querySelector('#replay').addEventListener("touchstart", function(event) {event.preventDefault();index = 0;queue()});
+document.getElementById('#shuffle').addEventListener("click", function(event) {event.preventDefault();buildList();index = 0;playList(itemList)});
+document.getElementById('#shuffle').addEventListener("touchstart", function(event) {event.preventDefault();buildList();index = 0;playList(itemList)});
+document.getElementById('#replay').addEventListener("click", function(event) {event.preventDefault();index = 0;queue()});
+document.getElementById('#replay').addEventListener("touchstart", function(event) {event.preventDefault();index = 0;queue()});
